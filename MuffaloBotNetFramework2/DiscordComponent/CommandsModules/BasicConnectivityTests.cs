93850ec41b9036b8d726063aefc0866d668d1cb8
Spdskatr <tunan.shi@gmail.com>
2017-11-29 22:18:52 +1100

    Added about

--- MuffaloBotNetFramework2/DiscordComponent/CommandsModules/BasicConnectivityTests.cs
+++ MuffaloBotNetFramework2/DiscordComponent/CommandsModules/BasicConnectivityTests.cs
@@ -23,5 +23,19 @@ namespace MuffaloBotNetFramework2.DiscordComponent.CommandsModules
             DiscordEmoji emoji = DiscordEmoji.FromName(ctx.Client, ":awoo:");
             await ctx.RespondAsync(emoji.ToString());
         }
+        [Command("about")]
+        public Task About(CommandContext ctx)
+        {
+            DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder();
+            embedBuilder.WithTitle("About MuffaloBot");
+            embedBuilder.WithUrl("https://github.com/spdskatr/MuffaloBot");
+            embedBuilder.WithDescription(@"Contributors: spdskatr
+Library: [DSharpPlus](https://dsharpplus.emzi0767.com/) (C#)
+Hosted by: Zirr
+GitHub Repository: https://github.com/spdskatr/MuffaloBot
+This bot account will not have an invite link. It is exclusive to a few guilds that have the bot.");
+            embedBuilder.WithColor(DiscordColor.Azure);
+            return ctx.RespondAsync(embed: embedBuilder.Build());
+        }
     }
 }
